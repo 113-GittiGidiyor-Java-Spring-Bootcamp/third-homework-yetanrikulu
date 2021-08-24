b3ddef22ba62c4881de9b778cbaf86175dd5423d
Yaemta <yaemta@gmail.com>
2021-08-24 22:49:45 +0300

    first commit

---
+++ School Management System/src/main/java/com/example/thirdhomeworkyetanrikulu/ThirdHomeworkYetanrikuluApplication.java
@@ -0,0 +1,13 @@
+package com.example.thirdhomeworkyetanrikulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdHomeworkYetanrikuluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdHomeworkYetanrikuluApplication.class, args);
+    }
+
+}
